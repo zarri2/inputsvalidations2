16bc31ca347fd9ece37b050ce138a9aa028c459f
zarri2 <168563126+zarri2@users.noreply.github.com>
2024-05-29 11:17:55 +0500

    updates

---
+++ Lesson1/src/App.jsx
@@ -0,0 +1,14 @@
+import Lesson1 from './Components/Lesson1';
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <Lesson1/>
+    </>
+  );
+}
+
+export default App
---
+++ Lesson1/src/Components/Lesson1.jsx
@@ -0,0 +1,31 @@
+export default function Lesson1() {
+    
+    return (
+        <>
+             {/* <h1 id="test">Lorem ipsum dolor sit amet.</h1>
+    
+    <p id="para">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit . Eos aspernatur facilis fuga illum eaque ut iste, veniam quas in autem eius, nobis facere quaerat labore?
+    </p> */} 
+    <p id="Question1"></p>
+    <p id="Question2"></p>
+    <p id="Question3"></p>
+    <h3> Phone number checker </h3>
+    <p id="Q4.statment1"></p>
+    <p id="Q4.statment2"></p>
+    <p id="Q4.statment3"></p>
+    <p id="Power"></p>
+    <p id="Question6"></p>
+
+    <p id="Question7"></p>
+
+    <div>
+        <p id="Question8a"></p>
+        <p id="Question8b"></p>
+
+    </div>
+        
+        </>
+    );
+}
+ 
